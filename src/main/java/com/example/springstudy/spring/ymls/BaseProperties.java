9a255f158e00556cb973b4280e246c7781449f1b
심규남 <ppp9026@gmail.com>
2022-04-23 11:19:25 +0900

    yml 학습코드

--- src/main/java/com/example/springstudy/spring/ymls/BaseProperties.java
+++ src/main/java/com/example/springstudy/spring/ymls/BaseProperties.java
@@ -3,6 +3,9 @@ package com.example.springstudy.spring.ymls;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
