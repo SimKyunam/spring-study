918fd7157214ebebe2b46df7dd4c242d50a0eec6
심규남 <ppp9026@gmail.com>
2022-05-05 21:54:32 +0900

    모듈 추가

---
+++ toby-controller/src/main/java/com/example/tobycontroller/TobyControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.tobycontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobyControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobyControllerApplication.class, args);
+    }
+
+}
---
+++ toby-controller/src/test/java/com/example/tobycontroller/TobyControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tobycontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobyControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
